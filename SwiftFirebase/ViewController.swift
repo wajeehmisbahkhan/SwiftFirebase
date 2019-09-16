c72f44042c0237752b8c06ee157664d340d752af
Wajeeh Misbah Khan <wajeehmisbahkhan@yahoo.com>
2019-09-16 22:49:55 +0500

    Initial Commit

---
+++ SwiftFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFirebase
+//
+//  Created by Wajeeh Misbah Khan on 16/09/2019.
+//  Copyright © 2019 Telic Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
